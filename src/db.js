83bb5e02e10a5b687507b26810ae5232dddab4ff
Your Name <julian.sonido@gmail.com>
2022-12-07 14:41:59 -0300

    deploy

--- src/db.js
+++ src/db.js
@@ -16,9 +16,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
   }
 ); */
 
-const sequelize = new Sequelize(
-  DB_DEPLOY, // Se cambia esto nomas trayendo la base de datos de .env que cree en railway previamente
-  {
+const sequelize = new Sequelize(DB_DEPLOY, {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
